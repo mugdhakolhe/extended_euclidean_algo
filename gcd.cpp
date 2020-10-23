7640213773e4b4188d299745318614bee8f6d767
Mugdha Kolhe <39653137+mugdhakolhe@users.noreply.github.com>
2020-10-24 02:20:53 +0530

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int gcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x_coef, y_coef;
+    int d = gcd(b, a % b, x_coef, y_coef);
+    x = y_coef;
+    y = x_coef - y_coef * (a / b);
+    return d;
+}
+
+int main(){
+	int a,b,x,y;
+	cin>>a>>b;
+	int gc=gcd(a,b,x,y);
+	int sum=x+y;
+	cout<<sum;
+	return 0;
+}
